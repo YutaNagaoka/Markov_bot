eceac5d03f2c1558648da5e3881cfc75fb003c5f
Yuta <shirasu.okatin@gmail.com>
2018-03-20 21:30:36 +0900

    [update]json2txt.py内のlist2str()の削除

--- json2txt.py
+++ json2txt.py
@@ -13,40 +13,23 @@ def make_vocab_file(input_dir_name):
     jsonファイルが入ったディレクトリを読み込んで形態素解析した単語を出力する
 
     :param input_dir_name -- 読み込むjsonファイルが入ったディレクトリ名 ※対話破綻コーパスのjsonファイルに限る
-
-    :param output_file_name -- 形態素解析した語を出力するテキストファイル名
     """
     input_dir = os.listdir(input_dir_name)
-    # output_file = open(output_file_name, "w", encoding="utf-8")
 
     for file in input_dir:
         path = os.path.join(input_dir_name, file)
+        # 入出力ファイルを開く
         with open(path, "r", encoding="utf-8") as f, \
                 open("Vocabulary/verb.txt", "w", encoding="utf-8") as v_file, \
                 open("Vocabulary/noun.txt", "w", encoding="utf-8") as n_file, \
                 open("Vocabulary/adjective.txt", "w", encoding="utf-8") as a_file:
             json_data = json.load(f)
+            # コーパスの一つの発話に対して
             for turn in json_data["turns"]:
                 token_list = tokenizer.tokenize(turn["utterance"])
                 classify_word(token_list, v_file, n_file, a_file)
 
-    # output_file.close()
     print("Succeeded!")
-
-
-def list2str(words):
-    """
-    形態素解析した語のリストを改行文字を区切り文字として連結し文字列を返す
-
-    :param words
-
-    :return: joined_words
-    """
-    # 以下の文字は除外する
-    exceptional_char = [',', '.', '、', '。', '?', '？', '!', '！', '～', 'ー', '「', '」']
-    words_list = [word.surface for word in words if word.surface not in exceptional_char]
-    joined_words = "\n".join(words_list)
-    return joined_words
 
 
 def classify_word(token_list, v_file, n_file, a_file):
@@ -69,7 +52,6 @@ def classify_word(token_list, v_file, n_file, a_file):
             n_file.write(token.surface + "\n")
         elif part == "形容詞":
             a_file.write(token.surface + "\n")
-
 
 
 if __name__ == '__main__':
