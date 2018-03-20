ede3a12ad7fdbad3fc1001e8c6c6c598e6a74d2e
Yuta <shirasu.okatin@gmail.com>
2018-03-20 20:10:15 +0900

    [update]bot.py のbot()関数をBot1クラスとして再実装
    管理の容易性のため

--- bot.py
+++ bot.py
@@ -1,24 +1,23 @@
 import random
 
 
-def bot(corpus, word_len):
-    """
-    マルコフ連鎖によって文章を作成する。
-    :param corpus: 単語ごとに開業したテキストファイル
-    :param word_len: 作成する文の単語数
-    """
-    vocab_file = open(corpus, "r", encoding="utf-8")
-    vocab_list = []
-    for vocab in vocab_file:
-        vocab_list.append(vocab)
+class Bot1:
+    def __init__(self, corpus, word_length):
+        self.corpus = corpus
+        self.word_length = word_length
 
-    for i in range(10):
-        utterance = random.sample(vocab_list, k=word_len)
-        utterance = [s.strip("\n") for s in utterance]
-        utterance = "".join(utterance)
-        print(utterance)
+    def utter(self):
+        vocab_file = open(self.corpus, "r", encoding="utf-8")
+        vocab_list = [vocab for vocab in vocab_file]
+
+        for i in range(10):
+            utterance = random.sample(vocab_list, k=self.word_length)
+            utterance = [s.strip("\n") for s in utterance]
+            utterance = "".join(utterance)
+            print(utterance)
 
 
 if __name__ == '__main__':
     word_len = 10
-    bot("vocabulary.txt", word_len)
+    bot1 = Bot1("vocabulary.txt", 10)
+    bot1.utter()
